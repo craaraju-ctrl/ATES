81ee5407fb277871872ea8abd16482126a7373b3
craaraju-ctrl <craaraju@gmail.com>
2026-06-08 13:56:18 +0530

    Create main_agents mod.rs

---
+++ crates/ates-agents/src/main_agents/mod.rs
@@ -0,0 +1,8 @@
+pub mod market_intelligence;
+pub mod risk_psychology;
+pub mod reflector;
+
+// Re-export
+pub use market_intelligence::MarketIntelligenceAgent;
+pub use risk_psychology::RiskPsychologyAgent;
+pub use reflector::ReflectorAgent;
